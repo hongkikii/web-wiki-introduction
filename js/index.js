88994bf0c2474c8caffa4568130d3407ff0dfedd
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-28 00:01:23 +0900

    chore: 댓글 기능 추가

--- js/index.js
+++ js/index.js
@@ -17,3 +17,37 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+function addComment() {
+  const commentInput = document.getElementById('comment-input');
+  const commentText = commentInput.value.trim(); // 입력된 댓글 내용
+
+  if (commentText === "") {
+    alert("댓글을 입력해주세요.");
+    return;
+  }
+
+  // 새로운 댓글 항목 생성
+  const newComment = document.createElement('li');
+  newComment.innerHTML = `
+    <div class="comment-item">
+      <div class="comment-author">
+        <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+        <span>방문자</span>
+      </div>
+      <div class="comment-content">
+        ${commentText}
+      </div>
+    </div>
+  `;
+
+  // 댓글 목록에 추가
+  const commentList = document.getElementById('comment-list');
+  commentList.appendChild(newComment);
+
+  // 댓글 입력창 비우기
+  commentInput.value = "";
+
+  // "댓글이 등록되었습니다" 메시지 알림
+  alert("댓글이 등록되었습니다.");
+}
